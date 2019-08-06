95064957e7a5fe69d7480fcbbb46a89c91438e2d
lytv <ly.truong.dn@gmail.com>
2019-08-06 21:01:25 +0700

    them file bai 3 cho git

---
+++ bai3.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# khi tao moi thi khong thuc thi duoc nen phai doi mode cho no : chmod +x file.sh
+
+echo "Nha ten ban"
+read name1 name2 name3
+echo "Nhap ten cua ban : $name1 ,$name2, $name3"
+
+
