313db8f05485a73355c12491a104500505dd8baa
lytv <ly.truong.dn@gmail.com>
2019-08-07 11:19:21 +0700

    them lenh swarm

---
+++ openportswarm.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+sudo ufw allow 22/tcp
+sudo ufw allow 2376/tcp
+sudo ufw allow 2377/tcp
+sudo ufw allow 7946/tcp
+sudo ufw allow 7946/udp
+sudo ufw allow 4789/udp
+
+sudo ufw enable
+
+sudo systemctl restart docker
---
+++ openportswarmworker.sh
@@ -0,0 +1,14 @@
+
+#! /bin/bash
+
+sudo ufw allow 22/tcp
+sudo ufw allow 2376/tcp
+sudo ufw allow 7946/tcp 
+sudo ufw allow 7946/udp 
+sudo ufw allow 4789/udp 
+
+sudo ufw enable
+
+sudo systemctl restart docker
+
+
